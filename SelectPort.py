97c74b4780c6bb8686ef62551adfe60f303dc8c9
TwilightAspect <aamoy013@uottawa.ca>
2026-07-07 18:31:12 -0400

    Program tells user which port is connected to the arduino and printer if present

--- SelectPort.py
+++ SelectPort.py
@@ -9,7 +9,7 @@ global port_dict
 port_dict = {}
 #lists available ports in new window and allows user to select from given ports
 def selectport(root):
-    print(str(serial.tools.list_ports.comports())) 
+    # print(str(serial.tools.list_ports.comports())) 
     global frm
     frm = Toplevel(root)
     # frm = ttk.Frame(root, padding=100,height=200,width=500)
@@ -17,18 +17,23 @@ def selectport(root):
     ttk.Label(frm, text="Select Arduino Port").grid(column=0, row=0)
     ttk.Label(frm, text="Select Printer Port").grid(column=0, row=1)
     
+    comports = serial.tools.list_ports.comports()
+
     global port_dict
     port_list = []
+    portid_list = []
     port_dict = {}
     #populate port_list and port_dict
-    for i in serial.tools.list_ports.comports():
+    for i in comports:
         port_id = i.device
         port_str = i.device
-        if port_str.__eq__(constvals.discovered_arduino):
+        # print(constvals.discovered_arduino)
+        if constvals.discovered_arduino != None and port_str.__eq__(constvals.discovered_arduino):
             port_str += " (Arduino)"
-        if port_str.__eq__(constvals.discovered_printer):
+        if constvals.discovered_printer != None and port_str.__eq__(constvals.discovered_printer):
             port_str += " (3D Printer)"
         port_list.append(port_str)
+        portid_list.append(port_id)
         port_dict.update({port_str:port_id})
 
     #Dropdown menu
@@ -38,8 +43,17 @@ def selectport(root):
     options_dict = dict(options)
     arduino_option = StringVar()
     printer_option = StringVar()
-    arduino_option.set(options["arduino_port"])
-    printer_option.set(options["printer_port"])
+    if constvals.discovered_arduino != None:
+        arduino_option.set(port_list[portid_list.index(constvals.discovered_arduino)])
+    else:
+        if options[constvals.OPTIONS_ARDUINO] in portid_list:
+            arduino_option.set(port_list[portid_list.index(options["arduino_port"])])
+    
+    if constvals.discovered_printer != None:
+        printer_option.set(port_list[portid_list.index(constvals.discovered_printer)])
+    else:
+        if options[constvals.OPTIONS_PRINTER] in portid_list:
+            printer_option.set(port_list[portid_list.index(options["printer_port"])])
     print(printer_option.get())
     print(arduino_option.get())
     OptionMenu(frm,arduino_option,*port_list).grid(column=2,row=0)
@@ -54,9 +68,14 @@ def selectport(root):
 
 def confirmport(frm):
     global port_dict
-    if arduino_option.get() == printer_option.get():
+    if arduino_option.get() == "" or printer_option.get() == "":
+        UtilUI.tooltip("You must select a port for both devices",autoclose=True,close_time=2)
+        return
+
+    if port_dict[arduino_option.get()] == port_dict[printer_option.get()]:
         UtilUI.tooltip("Arduino Port and Printer Port can NOT be the same",autoclose=True,close_time=2)
         return
+    print(f"Printer option: '{printer_option.get()}'")
     options_dict.update({"arduino_port":port_dict[arduino_option.get()]})
     options_dict.update({"printer_port":port_dict[printer_option.get()]})
 
